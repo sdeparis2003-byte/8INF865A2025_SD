ccaf11f79e80486520e2e8b4d7ebf051bab46868
sdeparis2003-byte <sdeparis2003@gmail.com>
2025-10-19 11:25:08 -0400

    Etape9: architecture des applications

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import com.example.unscramble.data.SCORE_INCREASE
 import kotlinx.coroutines.flow.update
+import com.example.unscramble.data.MAX_NO_OF_WORDS
 
 class GameViewModel : ViewModel() {
     var userGuess by mutableStateOf("")
@@ -74,13 +75,25 @@ class GameViewModel : ViewModel() {
 
 
     private fun updateGameState(updatedScore: Int) {
-        _uiState.update { currentState ->
-            currentState.copy(
-                isGuessedWordWrong = false,
-                currentScrambledWord = pickRandomWordAndShuffle(),
-                score = updatedScore ,
-                currentWordCount = currentState.currentWordCount.inc(),
-            )
+        if (usedWords.size == MAX_NO_OF_WORDS){
+            //Last round in the game, update isGameOver to true, don't pick a new word
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    score = updatedScore,
+                    isGameOver = true
+                )
+            }
+        } else{
+            // Normal round in the game
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    currentScrambledWord = pickRandomWordAndShuffle(),
+                    currentWordCount = currentState.currentWordCount.inc(),
+                    score = updatedScore
+                )
+            }
         }
     }
     fun skipWord() {
@@ -88,7 +101,6 @@ class GameViewModel : ViewModel() {
         // Reset user guess
         updateUserGuess("")
     }
-
 
 }
 
